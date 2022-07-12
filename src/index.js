376ffe328e4263b8ed2482cb529680f849844080
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-07-12 11:52:42 +0300

    added Phaser 3

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App.jsx";
+const config = {
+	type: Phaser.AUTO,
+	parent: "phaser-example",
+	width: 800,
+	height: 600,
+	scene: {
+		preload: preload,
+		create: create,
+	},
+};
+const game = new Phaser.Game(config);
+function preload() {
+	this.load.image("logo", logoImg);
+}
+function create() {
+	const logo = this.add.image(400, 150, "logo");
+	this.tweens.add({
+		targets: logo,
+		y: 450,
+		duration: 2000,
+		ease: "Power2",
+		yoyo: true,
+		loop: -1,
+	});
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
