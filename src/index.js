df8bf9c8be3f440303a6e6265428976907dfaf20
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-07-12 14:32:48 +0300

    temp

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
 import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App.jsx';
-const config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create,
-  },
-};
-const game = new Phaser.Game(config);
-console.log(game);
-function preload() {
-  this.load.image('logo', logoImg);
-}
-function create() {
-  const logo = this.add.image(400, 150, 'logo');
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: 'Power2',
-    yoyo: true,
-    loop: -1,
-  });
-}
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-root.render(
-  <StrictMode>
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
