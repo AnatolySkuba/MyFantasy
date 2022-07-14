3f177d5b7bcc16a48444a3edde40c19048a7cfec
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-07-14 10:59:44 +0300

    fixed path to assets

--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -5,16 +5,16 @@ export class LoadingScene extends Scene {
 		super("loading-scene");
 	}
 	preload(): void {
-		// if (window.innerHeight > 768) {
-		// 	this.load.atlas("sprite", "../../assets/spriteDesktop.png", "../../assets/spriteDesktop.json");
-		// } else {
-		// 	this.load.atlas("sprite", "../../assets/spriteMobile.png", "../../assets/spriteMobile.json");
-		// }
 		if (window.innerHeight > 768) {
 			this.load.atlas("sprite", "/MyFantasy/assets/spriteDesktop.png", "/MyFantasy/assets/spriteDesktop.json");
 		} else {
 			this.load.atlas("sprite", "/MyFantasy/assets/spriteMobile.png", "/MyFantasy/assets/spriteMobile.json");
 		}
+		// if (window.innerHeight > 768) {
+		// 	this.load.atlas("sprite", "/assets/spriteDesktop.png", "/assets/spriteDesktop.json");
+		// } else {
+		// 	this.load.atlas("sprite", "/assets/spriteMobile.png", "/assets/spriteMobile.json");
+		// }
 	}
 	create(): void {
 		this.scene.start("intro-scene");
